b67120a19e7a79b33f9e28d64d701a5edda149ac
Splice09 <jpf0005@gmail.com>
2015-12-08 14:22:59 -0600

    finish the feed forward logic inside each neuron and add a function calculation inside of each neuron (I used tanh)

--- NeuralNet/Neuron.cpp
+++ NeuralNet/Neuron.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 
 //Neuron Constructor
-Neuron::Neuron(unsigned numOutputs){
+Neuron::Neuron(unsigned numOutputs, unsigned myIndex){
 	//Here we are making a loop to add a Connection to the end of the outputWeight vector
 	//c stands for connections
 	for (unsigned c = 0; c < numOutputs; ++c){
@@ -10,12 +10,46 @@ Neuron::Neuron(unsigned numOutputs){
 		//assign a random weight to the weight stored in connection
 		m_outputWeights.back().weight = randomWeight();
 	}
+
+	m_myIndex = myIndex;
 }
 
+/*
+	Computs the math within the neuron
+*/
 void Neuron::feedForward(const Layer &previousLayer){
 	double sum = 0.0;
+	
+	// loop through and sum the product of the output and connection weight of each neuron from the previous layer
+	//  - include the bias node from the previous layer
+	for (unsigned n = 0; n < previousLayer.size(); ++n){
+		sum += previousLayer[n].getOutputValue() *
+			previousLayer[n].m_outputWeights[m_myIndex].weight;
+	}
+
+	//apply the activation function (transfer function) to shape neuron output
+	m_outputValue = Neuron::transferFunction(sum);
 }
 
+/*
+	Transfer function -- using a hyperbolic tangent function
+*/
+double Neuron::transferFunction(double x){
+	// tanh - output range [-1.0 .. 1.0]
+	return tanh(x);
+}
+
+/*
+	Derivative of transfer function
+*/
+double Neuron::transferFunctionDerivative(double x){
+	//tanh derivative approximation
+	return 1.0 - x * x;
+}
+
+/*
+	Random weight generator
+*/
 double Neuron::randomWeight(void){
 	return  std::rand() / double(RAND_MAX);
 }
@@ -28,6 +62,6 @@ void Neuron::setOutputValue(double value){
 	m_outputValue = value;
 }
 
-double Neuron::getOutputValue(void){
+double Neuron::getOutputValue(void) const{
 	return m_outputValue;
 }
--- NeuralNet/Neuron.h
+++ NeuralNet/Neuron.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <cmath>
+class Neuron;
 
 typedef std::vector<Neuron> Layer;
 
@@ -12,15 +14,18 @@ class Neuron
 {
 public:
 	//pass in the number of neurons in the next layer so we know how many output connections to make
-	Neuron(unsigned numOutputs);
-	static double randomWeight(void);
+	Neuron(unsigned numOutputs, unsigned myIndex);
 	void setOutputValue(double value);
-	double getOutputValue(void);
+	double getOutputValue(void) const;
 	void feedForward(const Layer &previousLayer);
 
 
 private:
+	static double randomWeight(void);
+	static double transferFunction(double x);
+	static double transferFunctionDerivative(double x);
 	double m_outputValue;
 	std::vector<Connection> m_outputWeights;
+	unsigned m_myIndex;
 };
 
